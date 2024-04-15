e9efdae6046cf4dcc623d747ca8b39655af4fdac
HaNguyenAcstone <actsonevnAsus@gmail.com>
2024-04-15 17:07:13 +0700

    firt project

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+
+}
+
+func pushOrders(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Orders pushed to successfully",
+		"status":  "OK",
+	})
+}
+
+func main() {
+	r := gin.Default()
+
+	// Thiết lập route
+	r.GET("/push_orders", pushOrders)
+
+	// Khởi chạy server
+	r.Run(":5000")
+}
